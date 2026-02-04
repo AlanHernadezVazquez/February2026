adc74a12505c8ad1755a223eceb99112dffcbb1e
AlanHV <25AlanHernadezVazquez.acc@lwetb.ie>
2026-02-04 15:35:00 +0000

    Update Fahrenheit to Celsius and Celsius to Fahrenheit February 4 2026.py

--- Fahrenheit to Celsius and Celsius to Fahrenheit February 4 2026.py
+++ Fahrenheit to Celsius and Celsius to Fahrenheit February 4 2026.py
@@ -2,7 +2,7 @@ QUESTION = input('Do you want to convert Fahrenheit to Celsius or Celsius to Fah
 if QUESTION == 'Fahrenheit to Celsius':
     Fahrenheit = input('Enter Fahrenheit: ')
     Fahrenheit = float(Fahrenheit)
-    Celsius = ((Fahrenheit -32) *1.8)
+    Celsius = ((Fahrenheit -32) *0.55555555555)
     print('The Celsius of '+str(Fahrenheit)+'°F is '+str(Celsius)+'°C')
 elif QUESTION == 'Celsius to Fahrenheit':
     Celsius1 = input('Enter Celsius: ')
@@ -10,4 +10,5 @@ elif QUESTION == 'Celsius to Fahrenheit':
     Fahrenheit1 = ((Celsius1 * 1.8) +32)
     print('The Fahrenheit of '+str(Celsius1)+'°C is '+str(Fahrenheit1)+'°F')
 else:
+
     print('ERROR')
